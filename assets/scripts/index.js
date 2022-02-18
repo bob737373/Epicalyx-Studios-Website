15e2b03a51b4f88dd44753b0ab0e8d73abcfaa46
SamJones329 <55857337+SamJones329@users.noreply.github.com>
2022-02-18 17:57:34 -0600

    commented out support buttons

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,11 +1,10 @@
 window.onload = function () {
-    const supportLinks = document.querySelectorAll('a.support-link');
+    // const supportLinks = document.querySelectorAll('a.support-link');
 
-    supportLinks.forEach( link => {
-        //link.addEventListener('click', () => {
-        link.onclick = () => {
-            alert("Sorry, this link is not setup yet, please try again later.");
-        };
-    });
-    console.log(supportLinks.values().next())
+    // supportLinks.forEach( link => {
+    //     //link.addEventListener('click', () => {
+    //     link.onclick = () => {
+    //         alert("Sorry, this link is not setup yet, please try again later.");
+    //     };
+    // });
 };
